d49c431033ecb5323ae20b3b282262b2a96b23f8
Daniel.Pawlikowski <Daniel.Pawlikowski@PK3-03>
2022-03-10 10:09:29 +0100

    Dodaj pliki projektów.

---
+++ Intel 8086/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Intel_8086
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj AH");
+            int ah = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj AL");
+            int al = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj BH");
+            int bh = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj BL");
+            int bl = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj CH");
+            int ch = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj CL");
+            int cl = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj DH");
+            int dh = isHex(Console.ReadLine());
+            Console.WriteLine("Podaj DL");
+            int dl = isHex(Console.ReadLine());
+            Console.WriteLine($"{ah},{al},{bl}");
+        }
+
+        static int isHex(string hex)
+        {
+                int hexInt = Convert.ToInt32(hex, 16);
+                return hexInt;
+        }
+    }
+}
