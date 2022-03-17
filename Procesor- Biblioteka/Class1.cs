8dcddbe3285c032f426f26475d36e1014545b114
danioper <danio4242@gmail.com>
2022-03-17 11:22:05 +0100

    DZIAŁA KONSOLOWO POZDRO

--- Procesor- Biblioteka/Class1.cs
+++ Procesor- Biblioteka/Class1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Linq;
 namespace Procesor__Biblioteka
 {
     public class Intel8086
@@ -18,20 +18,31 @@ namespace Procesor__Biblioteka
             }
         }
     }
+    public class Rejest
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Rejest(string n, int v)
+        {
+            this.Name = n;
+            this.Value = v;
+        }
+    }
 
     public class Procesor
     {
-        public int[] rejestry = new int[8];
-
+        public Rejest[] rejestr;
         public Procesor(){
-            rejestry[0] = Intel8086.IsHex();
-            rejestry[1] = Intel8086.IsHex();
-            rejestry[2] = Intel8086.IsHex();
-            rejestry[3] = Intel8086.IsHex();
-            rejestry[4] = Intel8086.IsHex();
-            rejestry[5] = Intel8086.IsHex();
-            rejestry[6] = Intel8086.IsHex();
-            rejestry[7] = Intel8086.IsHex();
+            rejestr = new Rejest[8];
+            rejestr[0] = new Rejest("AL", Intel8086.IsHex());
+            rejestr[1] = new Rejest("AH", Intel8086.IsHex());
+            rejestr[2] = new Rejest("BL", Intel8086.IsHex());
+            rejestr[3] = new Rejest("BH", Intel8086.IsHex());
+            rejestr[4] = new Rejest("CL", Intel8086.IsHex());
+            rejestr[5] = new Rejest("CH", Intel8086.IsHex());
+            rejestr[6] = new Rejest("DL", Intel8086.IsHex());
+            rejestr[7] = new Rejest("DH", Intel8086.IsHex());
             WhatToDo();
         }
 
@@ -41,7 +52,6 @@ namespace Procesor__Biblioteka
             switch(program)
             {
                 case "MOV":
-                    Console.WriteLine("HEJ");
                         Mov();
                     break;
                 case "XCH":
@@ -57,20 +67,59 @@ namespace Procesor__Biblioteka
 
         public int Mov()
         {
-            return 0;
+            string x = Console.ReadLine().ToUpper();
+            string y = Console.ReadLine().ToUpper();
+
+            if (FindFromRejest(x) == -1 || FindFromRejest(y) == -1)
+            {
+                Console.WriteLine("Jeden z podanych rejestrów jest nieprawidłowy, podaj je ponownie");
+                Mov();
+            };
+
+            return rejestr[FindFromRejest(x)].Value = rejestr[FindFromRejest(y)].Value;
+
         }
 
-        public int XCH()
+        public int? XCH()
         {
-            return 0;
+            string x = Console.ReadLine().ToUpper();
+            string y = Console.ReadLine().ToUpper();
+
+            if (FindFromRejest(x) == -1 || FindFromRejest(y) == -1)
+            {
+                Console.WriteLine("Jeden z podanych rejestrów jest nieprawidłowy, podaj je ponownie");
+                Mov();
+            };
+            int tmp = rejestr[FindFromRejest(x)].Value;
+            rejestr[FindFromRejest(x)].Value = rejestr[FindFromRejest(y)].Value;
+            rejestr[FindFromRejest(y)].Value = tmp;
+
+            return null;
+        }
+
+        public int FindFromRejest(string rejestrName)
+        {
+            switch (rejestrName)
+            {
+                case "AL": return 0;
+                case "AH": return 1;
+                case "BL": return 2;
+                case "BH": return 3;
+                case "CL": return 4;
+                case "CH": return 5;
+                case "DL": return 6;
+                case "DH": return 7;
+                default: return -1;
+            }
         }
 
         public override string ToString()
         {
-            return $" AL = {Convert.ToString(rejestry[0], 16).ToUpper()}   AH = {Convert.ToString(rejestry[1], 16).ToUpper()}\n" +
-                   $" BL = {Convert.ToString(rejestry[2], 16).ToUpper()}   BH = {Convert.ToString(rejestry[3], 16).ToUpper()}\n" +
-                   $" CL = {Convert.ToString(rejestry[4], 16).ToUpper()}   CH = {Convert.ToString(rejestry[5], 16).ToUpper()}\n" +
-                   $" DL = {Convert.ToString(rejestry[6], 16).ToUpper()}   DH = {Convert.ToString(rejestry[7], 16).ToUpper()}\n";
+            return $" AL = {Convert.ToString(rejestr[0].Value, 16).ToUpper()}   AH = {Convert.ToString(rejestr[1].Value, 16).ToUpper()}\n" +
+                   $" BL = {Convert.ToString(rejestr[2].Value, 16).ToUpper()}   BH = {Convert.ToString(rejestr[3].Value, 16).ToUpper()}\n" +
+                   $" CL = {Convert.ToString(rejestr[4].Value, 16).ToUpper()}   CH = {Convert.ToString(rejestr[5].Value, 16).ToUpper()}\n" +
+                   $" DL = {Convert.ToString(rejestr[6].Value, 16).ToUpper()}   DH = {Convert.ToString(rejestr[7].Value, 16).ToUpper()}\n";
         }
     }
+
 }
